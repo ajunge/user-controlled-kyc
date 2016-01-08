74169d0d066c5dcf01129eee7579cbb3f43dcd49
Andres Junge <ajunge@yaykuy.cl>
2016-01-08 17:34:19 -0300

    login with seed

--- app/javascripts/Login.jsx
+++ app/javascripts/Login.jsx
@@ -2,7 +2,7 @@
 
 var Login = React.createClass({
 	getInitialState: function() {
-	    return {error: ''};
+			return {seedInput: '', error: ''};
 	},
 	contextTypes: {
 		history: React.PropTypes.object,
@@ -22,7 +22,7 @@ var Login = React.createClass({
 
 		KeystoreAPI.keystoreGet(identifier,token,
 			function(_postResult){
-				//console.log(_postResult);
+				  console.log(_postResult);
 			    var keystoreData = _postResult;
 			    this.props.setKeystoreData(keystoreData);
 				this.context.history.replaceState(null,'/dashboard');
@@ -38,11 +38,40 @@ var Login = React.createClass({
 		);
 	},
 
+	generateRandomSeed: function(){
+		var randomSeed = lightwallet.keystore.generateRandomSeed();
+		//console.log(randomSeed);
+		this.setState({seedInput: randomSeed});
+	},
+	handleSeedChange: function(event){
+		this.setState({seedInput: event.target.value});
+	},
+
+	setSeed: function(){
+		var seed=this.refs.seed.getValue();
+		var seedPassword = this.refs.seedPassword.getValue();
+		var ks = new lightwallet.keystore(seed, seedPassword);
+		ks.generateNewAddress(seedPassword);
+		var keystoreData = {
+			identifier: 'anonymous',
+			keystore: ks
+		}
+		this.props.setKeystoreData(keystoreData);
+		this.context.history.replaceState(null,'/dashboard');
+	},
+
+
   render: function() {
-	var footer = (
+	var footerKeyServer = (
 		<ButtonToolbar >
 			<Button className="pull-right" bsStyle='primary' onClick={this.goToRegister}>Register</Button>
 			<Button className="pull-right" bsStyle='primary' onClick={this.recover}>Login</Button>
+		</ButtonToolbar>
+	);
+	var footerSeed = (
+		<ButtonToolbar >
+			<Button className="pull-right" bsStyle='primary' onClick={this.generateRandomSeed}>Generate Seed</Button>
+			<Button className="pull-right" bsStyle='primary' onClick={this.setSeed}>Set Seed</Button>
 		</ButtonToolbar>
 	);
 	var errorAlert;
@@ -54,10 +83,21 @@ var Login = React.createClass({
         </Alert>
       );
     }
+
+		var seedInputStatus;
+		if(this.state.seedInput==''){
+			seedInputStatus='';
+		}else{
+			if(lightwallet.keystore.isSeedValid(this.state.seedInput)){
+				seedInputStatus='OK';
+			}else{
+				seedInputStatus='Bad';
+			}
+		}
     return (
     	<ReactBootstrap.Row>
-			<ReactBootstrap.Col md={6} mdOffset={3}>
-				<Panel header="Login" footer={ footer }>
+			<ReactBootstrap.Col md={4} mdOffset={2}>
+				<Panel header="Login using Keyserver" footer={ footerKeyServer }>
 		    		{errorAlert}
 		        	<form>
 		          	<Label>Username</Label>
@@ -65,6 +105,17 @@ var Login = React.createClass({
 		          	<Label>Password</Label>
 		        		<Input type="password" ref='password' />
 		          	</form>
+				</Panel>
+			</ReactBootstrap.Col>
+			<ReactBootstrap.Col md={4} mdOffset={0}>
+				<Panel header="Set offline seed" footer={ footerSeed }>
+		    		<form>
+		          	<Label>Seed</Label>
+									<Input type="text" ref='seed' value={this.state.seedInput}
+			        			onChange={this.handleSeedChange} addonAfter={seedInputStatus}/>
+									<Label>Local Password</Label>
+				        		<Input type="password" ref='seedPassword' />
+		        </form>
 				</Panel>
 			</ReactBootstrap.Col>
 		</ReactBootstrap.Row>
